c3a7227b7d00be471fe9a8d60e3a832ea8097ef1
grant <grantvanbreda@gmail.com>
2021-04-23 17:00:33 +1000

    Grant - Developer hello world

--- src/TradingPlatform/Test.java
+++ src/TradingPlatform/Test.java
@@ -3,5 +3,6 @@ package TradingPlatform;
 public class Test {
     private static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Developer Branch");
     }
 }
