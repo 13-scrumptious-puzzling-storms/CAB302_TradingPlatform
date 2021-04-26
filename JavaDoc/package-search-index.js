2df76f498137d0f648292dc8526305072ecf330d
grant <grantvanbreda@gmail.com>
2021-04-26 16:36:14 +1000

    Updated JavaDoc with new comments.

--- JavaDoc/package-search-index.js
+++ JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"TradingPlatform"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"TradingPlatform"},{"l":"TradingPlatform.NetworkProtocol"}];updateSearchResults();
