c982bdb6c44fda9148189b8e6eca49535d349b8b
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-04-26 16:07:18 +1000

    Added JDBCTradingPlatformDataSource class. This implements TradingPlatformDataSource and will manipulate the database.

---
+++ src/TradingPlatform/NetworkProtocol/JDBCTradingPlatformDataSource.java
@@ -0,0 +1,77 @@
+package TradingPlatform.NetworkProtocol;
+import TradingPlatform.OrganisationalUnit;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class JDBCTradingPlatformDataSource implements TradingPlatformDataSource {
+
+    private static final String INSERT_ORGANISATIONALUNIT = "INSERT INTO organisationalunit (name, credits) VALUES (?, ?);";
+    private static final String GET_ORGANISATIONALUNIT = "SELECT * FROM organisationalunit WHERE id=?";
+
+    private PreparedStatement addOrganisationalUnit;
+    private PreparedStatement getOrganisationalUnit;
+
+    private Connection connection;
+
+    /**
+     * Constructor connects to connection instance
+     * and prepares executable statements.
+     */
+    public JDBCTradingPlatformDataSource() {
+        connection = DBConnection.getInstance();
+        try {
+            Statement st = connection.createStatement();
+
+            // Preparing Statements
+            addOrganisationalUnit = connection.prepareStatement(INSERT_ORGANISATIONALUNIT);
+            getOrganisationalUnit = connection.prepareStatement(GET_ORGANISATIONALUNIT);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * @see TradingPlatform.NetworkProtocol.TradingPlatformDataSource#addOrganisationalUnit(TradingPlatform.OrganisationalUnit)
+     */
+    public void addOrganisationalUnit(OrganisationalUnit orgUnit) {
+
+    }
+
+    /**
+     * @see TradingPlatform.NetworkProtocol.TradingPlatformDataSource#getOrganisationalUnit(int)
+     */
+    public OrganisationalUnit getOrganisationalUnit(int id) {
+        OrganisationalUnit orgUnit = new OrganisationalUnit();
+        ResultSet rs = null;
+
+        try {
+            getOrganisationalUnit.setString(1, id);
+            rs = getOrganisationalUnit.executeQuery();
+            rs.next();
+
+            orgUnit.setId(rs.getInt("id"));
+            orgUnit.setName(rs.getString("name"));
+            orgUnit.setCredits(rs.getInt("credits"));
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return orgUnit;
+    }
+
+    /**
+     * @see TradingPlatform.NetworkProtocol.TradingPlatformDataSource#close()
+     */
+    public void close() {
+        try{
+            connection.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
