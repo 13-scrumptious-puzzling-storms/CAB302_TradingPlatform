a71f3b55610ba6cdaaade938ddbd9e36ec9c5716
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-07 16:07:19 +1000

    Added 'important' comment

--- src/TradingPlatform/NetworkProtocol/TradingPlatformDataSource.java
+++ src/TradingPlatform/NetworkProtocol/TradingPlatformDataSource.java
@@ -9,6 +9,12 @@ public interface TradingPlatformDataSource {
 
     // IS THIS SOMETHING WE WANT? DO WE WANT TO BAN DUPLICATE NAMES?
     // WOULD DUPLICATE NAMES BE A GOOD / BAD THING 'if it is not already in the table'
+    // I personally don't think duplicate names should be allowed. It's likely the user
+    // will only ever see trades for 'this organisational unit' and 'that organisational unit'
+    // they won't be faced with 'the trades for organisational unit ID:5 are ...' the actual
+    // name will probably be used whilst the ID used in the backend. As such, banning duplicate
+    // names will allow a user to know that the organisational unit they are investigating is
+    // unique and not multiple with the same name. - Liam
     /**
      * Adds an Organisational Unit to the organisational unit table
      * in the database, if it is not already in the table
