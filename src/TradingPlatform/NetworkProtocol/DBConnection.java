d88df3394735a04b934946528506490452bd3cdd
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-07 16:28:26 +1000

    Implemented DBConnection method and fields

--- src/TradingPlatform/NetworkProtocol/DBConnection.java
+++ src/TradingPlatform/NetworkProtocol/DBConnection.java
@@ -1,24 +1,52 @@
 package TradingPlatform.NetworkProtocol;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Establishes a connection with the database.
- *
+ * This class is extended upon the work of
  * @author Malcolm Corney
  */
 public class DBConnection {
+    private String propsFile = "./db.props";
 
     /**
      * The singleton instance of the database connection.
      */
-    private static Connection instance = null;
+    private static Connection instance;
 
     /**
      * Constructor initialises the connection.
      */
     private DBConnection() {
+        Properties props = new Properties();
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(propsFile);
+            props.load(inputStream);
+            inputStream.close();
 
+            // Specifying the data source, username and password.
+            String url = props.getProperty("jdbc.url");
+            String username = props.getProperty("jdbc.username");
+            String password = props.getProperty("jdbc.password");
+            String schema = props.getProperty("jdbc.schema");
+
+            // Get a connection
+            instance = DriverManager.getConnection(url + "/" + schema, username, password);
+        } catch (SQLException sqle) {
+            System.err.println(sqle);
+        } catch (FileNotFoundException fnfe) {
+            System.err.println(fnfe);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
