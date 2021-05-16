06fb0f76b9b331b9ff5cb806ef05ae2ab4bb4db3
Shanelle Haire <shanelle_ha@hotmail.com>
2021-05-16 16:35:24 +1000

    more Trade tests

--- src/TradingPlatform/UnitTests/TradeTests.java
+++ src/TradingPlatform/UnitTests/TradeTests.java
@@ -21,4 +21,22 @@ public class TradeTests {
         JDBCTradeDataSource val = new JDBCTradeDataSource(1, connection);
         assert (val.value(" ", 1) != 0);
     }
+
+    @Test
+    public void testGetQuantity(){
+        JDBCTradeDataSource quant = new JDBCTradeDataSource(1, connection);
+        assert (quant.getQuantity() == 0);
+    }
+
+    @Test
+    public void testGetAsset(){
+        JDBCTradeDataSource asset = new JDBCTradeDataSource(1, connection);
+        assert (asset.getAsset() != null);
+    }
+
+    @Test
+    public void testGetOrg(){
+        JDBCTradeDataSource org = new JDBCTradeDataSource(1, connection);
+        assert (org.getOrganisation() != null);
+    }
 }
