fc5bd49e01b4a13b0c9dd1da8bb4a35a31ce758b
Shanelle Haire <shanelle_ha@hotmail.com>
2021-05-18 20:15:55 +1000

    GUI panels

--- src/TradingPlatform/GUItester.java
+++ src/TradingPlatform/GUItester.java
@@ -17,23 +17,29 @@ public class GUItester extends JFrame {
     private static final long serialVersionUID = 692675871418401803L;
 
     public GUItester() {
-        super("Tabbed Pane Demo");
+        super("SPS Trading");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JTabbedPane pane = new JTabbedPane();
+        JTabbedPane pane2 = new JTabbedPane();
         JPanel panel1 = new JPanel();
-        panel1.add(new JLabel("Tab 1"));
         JPanel panel2 = new JPanel();
-        panel2.add(new JButton("Tab 2"));
+        JPanel panel2_1 = new JPanel();
+        JPanel panel2_2 = new JPanel();
         JPanel panel3 = new JPanel();
+        panel2.add(pane2);
+        panel1.add(new JLabel("Tab 1"));
+        panel3.add(new JButton("Tab 2"));
+        pane2.add("assets", panel2_1);
+        pane2.add("trades", panel2_2);
 
-        pane.add("This tab has a label", panel1);
-        pane.add("This tab has a button", panel2);
-        pane.add("This tab is empty", panel3);
+        pane.add("Home", panel1);
+        pane.add("Organisation Home", panel2);
+        pane.add("My Profile", panel3);
         getContentPane().add(pane);
 
         // Display the window.
-        setPreferredSize(new Dimension(300, 200));
+        setPreferredSize(new Dimension(500, 200));
         setLocation(new Point(100, 100));
         pack();
         setVisible(true);
