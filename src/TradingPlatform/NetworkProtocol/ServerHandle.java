b91deab7a9f918e16fd1b776cbd7261287060819
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-20 11:41:24 +1000

    ServerHandle can now deserialise an object sent from client. Unpack content, and retrieve requested information

--- src/TradingPlatform/NetworkProtocol/ServerHandle.java
+++ src/TradingPlatform/NetworkProtocol/ServerHandle.java
@@ -1,25 +1,41 @@
 package TradingPlatform.NetworkProtocol;
 
-import TradingPlatform.OrganisationalUnit;
+import TradingPlatform.JDBCDataSources.JDBCOrganisationalUnit;
+import TradingPlatform.Request;
 
 import java.io.*;
+import java.lang.invoke.VarHandle;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
 
-public class ServerHandle {
+public class ServerHandle extends Thread {
     private static final int PORT = 2197;
     private static final int BACKLOG = 25;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    private static Connection connection;
 
-        testRead();
+    @Override
+    public void run() {
+        try {
+            connection = DBConnection.getInstance();
+            getRequests();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static void getRequests() throws IOException, ClassNotFoundException {
         try (ServerSocket serverSocket = new ServerSocket(PORT, BACKLOG);){
             // Staying around for multiple connections
             for (;;) {
                 Socket socket = serverSocket.accept();
                 try(ObjectInputStream objectInputStream = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));) {
-                    ServerRequest clientRequest = (ServerRequest) objectInputStream.readObject();
+                    Object serialData = objectInputStream.readObject();
+                    Request clientRequest = (Request) serialData;
+                    System.out.println("serialData's ID: " + clientRequest.getClassName());
+                    System.out.println("serialData's Credits: " + clientRequest.getMethodName());
+                    System.out.println("serialData's Credits: " + clientRequest.getArguments()[0]);
                     handleRequest(clientRequest.getClassName(), clientRequest.getMethodName(), clientRequest.getArguments());
                 }
             }
@@ -27,11 +43,14 @@ public class ServerHandle {
     }
 
     private static void handleRequest(String className, String methodName, String[] arguments) {
+        System.out.println("Got request: " + className + " " + methodName + " " + arguments);
         switch (className) {
-            case "OrganisationalUnit":
+            case "OrganisationalUnitServer":
                 switch (methodName) {
                     case "getName":
                         // need to get Server Send working
+                        JDBCOrganisationalUnit DBInterface = new JDBCOrganisationalUnit(Integer.parseInt(arguments[0]), connection);
+                        System.out.println(DBInterface.getOrganisationalUnitName());
                         //ServerSend(OrganisationalUnitServer.getName(Integer.parseInt(arguments[0])));
                         //ServerSend ...;
                         break;
@@ -40,7 +59,9 @@ public class ServerHandle {
                         break;
                     default:
                         System.out.println("Invalid Method");
+                        break;
                 }
+                break;
             case "OrganisationAsset":
                 switch (methodName) {
                     case "getQuantity":
@@ -51,19 +72,24 @@ public class ServerHandle {
                         break;
                     default:
                         System.out.println("Invalid Method");
+                        break;
                 }
+                break;
             default:
                 System.out.println("Invalid Class");
+                break;
         }
     }
 
-    private static void testRead() throws FileNotFoundException {
-        //File filename = new File("mySerialData");
+    private static void testRead() throws IOException {
+        File filename = new File("mySerialData");
 
-        //try (ObjectInputStream objectInputStream = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filename)));) {
-        //    OrganisationalUnitServer serialData = (OrganisationalUnitServer) objectInputStream.readObject();
-        //    System.out.println("serialData's ID: " + serialData.getID());
-        //    System.out.println("serialData's Credits: " + serialData.getCredits());
-        //}
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filename)));) {
+            OrganisationalUnitServer serialData = (OrganisationalUnitServer) objectInputStream.readObject();
+            System.out.println("serialData's ID: " + serialData.getID());
+            System.out.println("serialData's Credits: " + serialData.getCredits());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
