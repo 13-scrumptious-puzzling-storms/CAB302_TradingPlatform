ef276a135f2152e7fc510ca54923e61f016e9236
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-20 11:43:59 +1000

    Improved and renamed to simply Request. Both client and server will have identical copies of these.

--- src/TradingPlatform/Request.java
+++ src/TradingPlatform/Request.java
@@ -2,21 +2,25 @@ package TradingPlatform;
 
 import java.io.Serializable;
 
-public class ClientRequest implements Serializable {
+public class Request implements Serializable {
     private static final long serialVersionUID = -2289891509187701177L;
 
     private String className;
     private String methodName;
     private String[] arguments;
 
-    public ClientRequest(String className, String methodName) {
+    public Request(String className, String methodName) {
         this.className = className;
         this.methodName = methodName;
         this.arguments = null;
     }
 
-    public ClientRequest(String className, String methodName, String[] arguments) {
+    public Request(String className, String methodName, String[] arguments) {
         this(className, methodName);
         this.arguments = arguments;
     }
+
+    public String getClassName() { return className; }
+    public String getMethodName() { return methodName; }
+    public String[] getArguments() { return  arguments; }
 }
