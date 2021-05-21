d878eca5668343ba2b24d6baeaeb88cc66738b4d
grant <grantvanbreda@gmail.com>
2021-05-21 18:48:33 +1000

    Implemented the database methods for getting current buy/sell orders for an asset type.

--- src/TradingPlatform/Interfaces/TradeReconcileSource.java
+++ src/TradingPlatform/Interfaces/TradeReconcileSource.java
@@ -1,6 +1,10 @@
 package TradingPlatform.Interfaces;
 
+import TradingPlatform.AssetType;
+import TradingPlatform.TradeReconciliation.TradeOrder;
 import TradingPlatform.TradeReconciliation.TradeRecon;
+
+import java.util.ArrayList;
 
 public interface TradeReconcileSource {
     /**
@@ -8,4 +12,24 @@ public interface TradeReconcileSource {
      * @param recon the reconciliation that will be inserted
      */
     void insertTradeRecon(TradeRecon recon);
+
+    /**
+     * Gets the current sell orders for an asset type ordered by Price then CreatedTime
+     * @param assetTypeId The assetTypeId of the buy order
+     * @return An arraylist of trade orders
+     */
+    ArrayList<TradeOrder> getCurrentSellOrders(int assetTypeId);
+
+    /**
+     * Gets the current buy orders for an asset type ordered by Price then CreatedTime
+     * @param assetTypeId The assetTypeId of the buy order
+     * @return An arraylist of trade orders
+     */
+    ArrayList<TradeOrder> getCurrentBuyOrders(int assetTypeId);
+
+    /**
+     * Gets the current asset types that can be reconciled (the asset types have pending reconcilable trades)
+     * @return An arraylist of AssetTypes that have pending reconcilable trades
+     */
+    ArrayList<Integer> getCurrentReconcilableAssetTypeIds();
 }
