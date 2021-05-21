e023e048d0140b656118d76b9093502f90e5de79
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-21 13:00:57 +1000

    Request.java is the object that is serialised and deserialised for client-server protocol.

--- src/TradingPlatform/Request.java
+++ src/TradingPlatform/Request.java
@@ -8,11 +8,18 @@ public class Request implements Serializable {
     private String className;
     private String methodName;
     private String[] arguments;
+    private Boolean response;
 
     public Request(String className, String methodName) {
         this.className = className;
         this.methodName = methodName;
         this.arguments = null;
+        this.response = false;
+    }
+
+    public Request(String className, String methodName, Boolean response) {
+        this(className, methodName);
+        this.response = response;
     }
 
     public Request(String className, String methodName, String[] arguments) {
@@ -20,7 +27,13 @@ public class Request implements Serializable {
         this.arguments = arguments;
     }
 
+    public Request(String className, String methodName, String[] arguments, Boolean response) {
+        this(className, methodName, arguments);
+        this.response = response;
+    }
+
     public String getClassName() { return className; }
     public String getMethodName() { return methodName; }
     public String[] getArguments() { return  arguments; }
+    public Boolean getResponse() { return  response; }
 }
