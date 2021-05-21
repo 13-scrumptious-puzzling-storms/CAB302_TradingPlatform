5c05a80686232ecdfcb86a2844754164b2cfd948
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-21 16:37:06 +1000

    Initialise NetworkManager.java on separate thread

--- src/TradingPlatform/ClientApp.java
+++ src/TradingPlatform/ClientApp.java
@@ -3,20 +3,19 @@ package TradingPlatform;
 import java.io.IOException;
 
 public class ClientApp {
-
-    // So you wanna talk to the serevr huh?!
-    // use this bad boy
     public static NetworkManager networkManager;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        initialiseClientSend();
-        testServerClient();
+        // Initialise the client-side network protocol
+        networkManager = new NetworkManager();
+        Thread networkThread = new Thread(networkManager);
+        networkThread.start();
+
+        networkTest();
         new GUItester();
     }
 
-    private static void initialiseClientSend() throws IOException { networkManager = new NetworkManager(); }
-
-    private static void testServerClient() throws IOException, ClassNotFoundException {
+    private static void networkTest() throws IOException, ClassNotFoundException {
         OrganisationalUnit organisationalUnit = new OrganisationalUnit();
         System.out.println("Organisational Unit Name for ID 1: " + organisationalUnit.getName(1));
     }
