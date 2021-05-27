c779b7b0b49f533f9ec44e0fc915d08e3989aae7
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-27 18:21:09 +1000

    Instantiates GUI on separate thread. Might need to use 'invokeLater' instead, will investigate later.

--- src/TradingPlatform/NetworkProtocol/ServerApp.java
+++ src/TradingPlatform/NetworkProtocol/ServerApp.java
@@ -1,11 +1,14 @@
 package TradingPlatform.NetworkProtocol;
 
-import java.io.*;
+import javax.swing.*;
+import java.io.IOException;
 
 public class ServerApp {
 
     private static ServerHandle serverHandleRunnable;
     public static ServerSend serverSendRunnable;
+
+    private static GUIManager GUIManagerRunnable;
 
     public static void main(String[] args) {
         System.out.println("Starting Server ...");
@@ -17,13 +20,28 @@ public class ServerApp {
         serverSendRunnable = new ServerSend();
         Thread threadSend = new Thread(serverSendRunnable);
         threadSend.start();
+
+        // NEED TO CHECK HOW TO PROPERLY START GUI. Do I use the invoke later below?
+        GUIManagerRunnable = new GUIManager();
+        Thread threadGUI = new Thread(GUIManagerRunnable);
+        threadGUI.start();
+
+        //SwingUtilities.invokeLater(new Runnable()
+        //{
+        //    public void run()
+        //    {
+        //        GUIManagerRunnable = new GUIManager();
+        //    }
+        //});
     }
 
     public static void shutdown() {
+        System.out.println("\nShutting down Server ...");
         // terminate all instances / get them to finish what they're doing
         // perhaps turn of the request handler
         serverHandleRunnable.end();
-        //System.exit(0);
+        // serverSendRunnable.end(); shouldn't be needed since it is not in a while loop. there is no boolean to flip anyway
+        System.exit(0);
         //could have sytem.exit after try block in thread
         // once bool is swithced AND thread finises what it was doing
         // then systrm .exit can be called
