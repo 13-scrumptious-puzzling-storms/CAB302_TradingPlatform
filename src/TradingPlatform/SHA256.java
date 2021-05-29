371ec0ac64259c6b67b8d4c1bb950fa80c49ec0e
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-29 15:42:22 +1000

    The SHA256 algorith to hash passwords

--- src/TradingPlatform/SHA256.java
+++ src/TradingPlatform/SHA256.java
@@ -4,11 +4,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA256 {
-    // Reference https://www.baeldung.com/sha-256-hashing-java
-    public static String hashPasword(String password) throws NoSuchAlgorithmException {
+
+    // Source https://www.baeldung.com/sha-256-hashing-java
+    public static String hashPassword(String password) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         messageDigest.update(password.getBytes());
         String stringHash = new String(messageDigest.digest());
         return stringHash;
+    }
+
+    public static void main(String[] args) {
+        String testEncode = "toor";
+        try { System.out.println(hashPassword(testEncode)); }
+        catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
     }
 }
