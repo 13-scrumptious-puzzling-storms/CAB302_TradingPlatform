6d3eb4538ee4fd79bfc7d38561c819d6fc756270
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-05-29 13:04:38 +1000

    Start GUILogin.java before main GUI

--- src/TradingPlatform/ClientApp.java
+++ src/TradingPlatform/ClientApp.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class ClientApp {
 
     public static NetworkManager networkManager;
+    private static GUILogin guiLogin;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         // Initialise the client-side network protocol
@@ -12,8 +13,13 @@ public class ClientApp {
         Thread networkThread = new Thread(networkManager);
         networkThread.start();
 
-        networkTest();
-        new GUIMain();
+        //networkTest();
+
+        guiLogin = new GUILogin();
+        Thread guiLoginThread = new Thread(guiLogin);
+        guiLoginThread.start();
+
+        //new GUIMain();
     }
 
     private static void networkTest() throws IOException, ClassNotFoundException {
