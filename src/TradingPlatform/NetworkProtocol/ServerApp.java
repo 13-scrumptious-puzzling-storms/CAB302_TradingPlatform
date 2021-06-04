0595ac805f382d84e54289ff9949cc1f6d9ed922
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-06-04 13:18:10 +1000

    Added Javadoc comments for class, constructor and methods. Also modified how Login GUI is invoked.

--- src/TradingPlatform/NetworkProtocol/ServerApp.java
+++ src/TradingPlatform/NetworkProtocol/ServerApp.java
@@ -1,5 +1,7 @@
 package TradingPlatform.NetworkProtocol;
 
+import TradingPlatform.GUILogin;
+
 import javax.swing.*;
 import java.io.IOException;
 
@@ -8,43 +10,31 @@ public class ServerApp {
     private static ServerHandle serverHandleRunnable;
     public static ServerSend serverSendRunnable;
 
-    private static GUIManager GUIManagerRunnable;
-
     public static void main(String[] args) {
         System.out.println("Starting Server ...");
 
+        // Start the Client requests handler for the Server.
         serverHandleRunnable = new ServerHandle();
         Thread threadHandle = new Thread(serverHandleRunnable);
         threadHandle.start();
 
+        // Start the Client reply component of the network protocol.
         serverSendRunnable = new ServerSend();
         Thread threadSend = new Thread(serverSendRunnable);
         threadSend.start();
 
-        // NEED TO CHECK HOW TO PROPERLY START GUI. Do I use the invoke later below?
-        GUIManagerRunnable = new GUIManager();
-        Thread threadGUI = new Thread(GUIManagerRunnable);
-        threadGUI.start();
-
-        //SwingUtilities.invokeLater(new Runnable()
-        //{
-        //    public void run()
-        //    {
-        //        GUIManagerRunnable = new GUIManager();
-        //    }
-        //});
+        // Start the Server GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUIManager();
+            }
+        });
     }
 
     public static void shutdown() {
         System.out.println("\nShutting down Server ...");
-        // terminate all instances / get them to finish what they're doing
-        // perhaps turn of the request handler
-        serverHandleRunnable.end();
-        // serverSendRunnable.end(); shouldn't be needed since it is not in a while loop. there is no boolean to flip anyway
+        serverHandleRunnable.end(); // Stop accepting requests
+        while (serverSendRunnable.isWorking) { } // Wait for ServerSend to finish
         System.exit(0);
-        //could have sytem.exit after try block in thread
-        // once bool is swithced AND thread finises what it was doing
-        // then systrm .exit can be called
-        // or maybe shutdown() can be called after the try block finishes?
     }
 }
