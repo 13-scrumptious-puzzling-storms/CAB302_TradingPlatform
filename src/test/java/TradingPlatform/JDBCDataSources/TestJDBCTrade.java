5d919f250bdc258038100f9bbde5091dbda5835b
Liam-Ferrante <liam.p.ferrante@gmail.com>
2021-06-06 22:27:59 +1000

    Removed unused import.

--- src/test/java/TradingPlatform/JDBCDataSources/TestJDBCTrade.java
+++ src/test/java/TradingPlatform/JDBCDataSources/TestJDBCTrade.java
@@ -1,6 +1,5 @@
 package TradingPlatform.JDBCDataSources;
 
-import TradingPlatform.Trade;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
